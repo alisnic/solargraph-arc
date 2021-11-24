c3affec37e7dc6c917e9be87c5ab3bd96aeb13b6
Andrei Lisnic <andrei.lisnic@gmail.com>
2021-11-24 11:50:25 +0200

    It works!

--- ast_demo.rb
+++ ast_demo.rb
@@ -1,7 +1,6 @@
 require 'solargraph'
+require 'active_support/core_ext/string/inflections'
 require 'pry'
-
-ast = Solargraph::Parser.parse_with_comments(File.read("db/schema.rb"), "db/schema.rb")
 
 class Walker
   class Hook
@@ -30,7 +29,7 @@ class Walker
     end
   end
 
-private
+  private
 
   def traverse(node)
     return unless node.is_a?(Parser::AST::Node)
@@ -64,26 +63,106 @@ private
   end
 end
 
-SCHEMA = {}
+class SolarRails < Solargraph::Convention::Base
+  RUBY_TYPES = {
+    decimal: 'BigDecimal',
+    integer: 'Integer',
+    date: 'Date',
+    datetime: 'ActiveSupport::TimeWithZone',
+    string: 'String',
+    boolean: 'Boolean',
+    text: 'String',
+    jsonb: 'Hash',
+    bigint: 'Integer',
+    inet: 'IPAddr'
+  }
 
-walker = Walker.new(ast)
-walker.on :block, [:send, nil, :create_table] do |ast, query|
-  table_name = ast.children.first.children[2].children.last
-  SCHEMA[table_name] = {}
+  def local source_map
+    if source_map.filename.include?("app/models")
+      Solargraph::Environ.new(pins: model_pins(source_map))
+      # pp model_pins(source_map)
+      # EMPTY_ENVIRON
+    else
+      EMPTY_ENVIRON
+    end
+  end
 
-  query.on :send, [:lvar, :t] do |column_ast|
-    name = column_ast.children[2].children.last
-    type = column_ast.children[1]
+  private
 
-    next if type == :index
-    SCHEMA[table_name][name] = type
+  def model_pins(source_map)
+    ns         = source_map.document_symbols.find {|s| s.is_a?(Solargraph::Pin::Namespace) }
+    table_name = infer_table_name(ns)
+    table      = schema[table_name]
+
+    return [] unless table
+
+    table.map do |column, type|
+      # TODO: get location data from db/schema.rb
+      location = Solargraph::Location.new(
+        "db/schema.rb",
+        Solargraph::Range.from_to(
+          0,
+          0,
+          0,
+          0
+        )
+      )
+
+      Solargraph::Pin::Method.new(
+        name:      column,
+        comments:  "@return [#{RUBY_TYPES.fetch(type.to_sym)}]",
+        location:  location,
+        closure:   ns,
+        scope:     :instance,
+        attribute: true
+      )
+    end
+  end
+
+  # TODO: support custom table names, by parsing `self.table_name = ` invokations
+  # inside model
+  def infer_table_name(ns)
+    ns.name.underscore.pluralize
+  end
+
+  def build_method_definition
+    Solargraph::Pin::Method.new(
+      name: attr[:name],
+      comments: "@return [#{type_translation.fetch(attr[:type], attr[:type])}]",
+      location: attr[:location],
+      closure: Solargraph::Pin::Namespace.new(name: module_names.join('::') + "::#{model_name}"),
+      scope: :instance,
+      attribute: true
+    )
+  end
+
+  def schema
+    @extracted_schema ||= begin
+      ast = Solargraph::Parser.parse_with_comments(File.read("db/schema.rb"), "db/schema.rb")
+      extract_schema(ast)
+    end
+  end
+
+  def extract_schema(ast)
+    schema = {}
+
+    walker = Walker.new(ast)
+    walker.on :block, [:send, nil, :create_table] do |ast, query|
+      table_name = ast.children.first.children[2].children.last
+      schema[table_name] = {}
+
+      query.on :send, [:lvar, :t] do |column_ast|
+        name = column_ast.children[2].children.last
+        type = column_ast.children[1]
+
+        next if type == :index
+        schema[table_name][name] = type
+      end
+    end
+
+    walker.walk
+    schema
   end
 end
 
-walker.walk
-
-pp SCHEMA
-
-binding.pry
-
-
+Solargraph::Convention.register SolarRails
