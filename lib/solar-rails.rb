4bf5c84e83b83e97aca9674cfacb50fba63d810b
Andrei Lisnic <andrei.lisnic@gmail.com>
2021-11-25 15:01:16 +0200

    Refactor

--- lib/solar-rails.rb
+++ lib/solar-rails.rb
@@ -73,202 +73,230 @@ class Walker
 end
 
 class SolarRails < Solargraph::Convention::Base
-  RUBY_TYPES = {
-    decimal: 'BigDecimal',
-    integer: 'Integer',
-    date: 'Date',
-    datetime: 'ActiveSupport::TimeWithZone',
-    string: 'String',
-    boolean: 'Boolean',
-    text: 'String',
-    jsonb: 'Hash',
-    bigint: 'Integer',
-    inet: 'IPAddr'
-  }
+  module Util
+    def self.build_public_method(ns, name, type, ast:, path:)
+      location = build_location(ast, path)
+
+      Solargraph::Pin::Method.new(
+        name:      name,
+        comments:  "@return [#{type}]",
+        location:  location,
+        closure:   ns,
+        scope:     :instance,
+        attribute: true
+      )
+    end
+
+    def self.build_location(ast, path)
+      Solargraph::Location.new(
+        path,
+        Solargraph::Range.from_to(
+          ast.location.first_line,
+          0,
+          ast.location.last_line,
+          ast.location.column
+        )
+      )
+    end
+  end
+
+  class Schema
+    ColumnData = Struct.new(:type, :ast)
+
+    RUBY_TYPES = {
+      decimal: 'BigDecimal',
+      integer: 'Integer',
+      date: 'Date',
+      datetime: 'ActiveSupport::TimeWithZone',
+      string: 'String',
+      boolean: 'Boolean',
+      text: 'String',
+      jsonb: 'Hash',
+      bigint: 'Integer',
+      inet: 'IPAddr'
+    }
+
+    def self.instance
+      @instance ||= self.new
+    end
+
+    def process(source_map, ns)
+      return [] unless source_map.filename.include?("app/models")
+
+      table_name = infer_table_name(ns)
+      table      = schema[table_name]
+
+      return [] unless table
+
+      table.map do |column, data|
+        Util.build_public_method(
+          ns,
+          column,
+          RUBY_TYPES.fetch(data.type.to_sym),
+          ast:  data.ast,
+          path: "db/schema.rb"
+        )
+      end
+    end
+
+    private
+
+    def schema
+      @extracted_schema ||= begin
+        ast = Solargraph::Parser.parse_with_comments(File.read("db/schema.rb"), "db/schema.rb")
+        extract_schema(ast)
+      end
+    end
+
+    # TODO: support custom table names, by parsing `self.table_name = ` invokations
+    # inside model
+    def infer_table_name(ns)
+      ns.name.underscore.pluralize
+    end
+
+    def extract_schema(ast)
+      schema = {}
+
+      walker = Walker.new(ast)
+      walker.on :block, [:send, nil, :create_table] do |ast, query|
+        table_name = ast.children.first.children[2].children.last
+        schema[table_name] = {}
+
+        query.on :send, [:lvar, :t] do |column_ast|
+          name = column_ast.children[2].children.last
+          type = column_ast.children[1]
+
+          next if type == :index
+          schema[table_name][name] = ColumnData.new(type, column_ast)
+        end
+      end
+
+      walker.walk
+      schema
+    end
+  end
+
+  class Relation
+    def self.instance
+      @instance ||= self.new
+    end
+
+    def process(source_map, ns)
+      return [] unless source_map.filename.include?("app/models")
+
+      ast    = source_map.source.node
+      walker = Walker.new(ast)
+      pins   = []
+
+      walker.on :send, [nil, :belongs_to] do |ast|
+        pins << singular_association(ns, ast)
+      end
+
+      walker.on :send, [nil, :has_one] do |ast|
+        pins << singular_association(ns, ast)
+      end
+
+      walker.on :send, [nil, :has_many] do |ast|
+        pins << plural_association(ns, ast)
+      end
+
+      walker.on :send, [nil, :has_and_belongs_to_many] do |ast|
+        pins << plural_association(ns, ast)
+      end
+
+      walker.walk
+      pins
+    end
+
+    # TODO: handle custom class for relation
+    def plural_association(ns, ast)
+      relation_name = ast.children[2].children.first
+
+      Util.build_public_method(
+        ns,
+        relation_name.to_s,
+        "ActiveRecord::Associations::CollectionProxy<#{relation_name.to_s.singularize.camelize}>",
+        ast:  ast,
+        path: ns.filename
+      )
+    end
+
+    # TODO: handle custom class for relation
+    def singular_association(ns, ast)
+      relation_name = ast.children[2].children.first
+
+      Util.build_public_method(
+        ns,
+        relation_name.to_s,
+        relation_name.to_s.camelize,
+        ast:  ast,
+        path: ns.filename
+      )
+    end
+  end
+
+  class NamespaceHack
+    def self.instance
+      @instance ||= self.new
+    end
+
+    def process(source_map, ns, ds)
+      return [] unless ds.size == 1 && ns.path.include?("::")
+      root_ns = source_map.pins.find {|p| p.path == "" }
+      namespace_stubs(root_ns, ns)
+    end
+
+    def namespace_stubs(root_ns, ns)
+      parts = ns.path.split("::")
+
+      candidates = parts.each_with_index.reduce([]) do |acc, (_, i)|
+        acc + [parts[0..i].join("::")]
+      end.reject {|el| el == ns.path }
+
+      previous_ns = root_ns
+      pins = []
+
+      parts[0..-2].each_with_index do |name, i|
+        gates = candidates[0..i].reverse + [""]
+        path = gates.first
+        next if path == ns.path
+
+        previous_ns = Solargraph::Pin::Namespace.new(
+          type:       :class,
+          location:   ns.location,
+          closure:    previous_ns,
+          name:       name,
+          comments:   ns.comments,
+          visibility: :public,
+          gates:      gates[1..-1]
+        )
+
+        pins << previous_ns
+      end
+
+      pins
+    end
+  end
 
   def local source_map
     pins = []
     ds   = source_map.document_symbols.select {|n| n.is_a?(Solargraph::Pin::Namespace) }
     ns   = ds.first
 
-    if ds.size == 1 && ns.path.include?("::")
-      root_ns = source_map.pins.find {|p| p.path == "" }
-      pins += namespace_stubs(root_ns, ns)
-    end
-
-    if source_map.filename.include?("app/models")
-      pins += model_pins(source_map)
-    end
+    pins += Schema.instance.process(source_map, ns)
+    pins += Relation.instance.process(source_map, ns)
+    pins += NamespaceHack.instance.process(source_map, ns, ds)
 
     Solargraph::Environ.new(pins: pins)
   end
 
   private
 
-  def namespace_stubs(root_ns, ns)
-    parts = ns.path.split("::")
-
-    candidates = parts.each_with_index.reduce([]) do |acc, (_, i)|
-      acc + [parts[0..i].join("::")]
-    end.reject {|el| el == ns.path }
-
-    previous_ns = root_ns
-    pins = []
-
-    parts[0..-2].each_with_index do |name, i|
-      gates = candidates[0..i].reverse + [""]
-      path = gates.first
-      next if path == ns.path
-
-      previous_ns = Solargraph::Pin::Namespace.new(
-        type:       :class,
-        location:   ns.location,
-        closure:    previous_ns,
-        name:       name,
-        comments:   ns.comments,
-        visibility: :public,
-        gates:      gates[1..-1]
-      )
-
-      pins << previous_ns
-    end
-
-    pins
-  end
 
   def model_pins(source_map)
     ns = source_map.document_symbols.find {|s| s.is_a?(Solargraph::Pin::Namespace) }
     schema_pins(ns) + relation_pins(source_map, ns)
   end
-
-  def schema_pins(ns)
-    table_name = infer_table_name(ns)
-    table      = schema[table_name]
-
-    return [] unless table
-
-    table.map do |column, data|
-      build_public_method(
-        ns,
-        column,
-        RUBY_TYPES.fetch(data.type.to_sym),
-        ast:  data.ast,
-        path: "db/schema.rb"
-      )
-    end
-  end
-
-  def build_public_method(ns, name, type, ast:, path:)
-    location = build_location(ast, path)
-
-    Solargraph::Pin::Method.new(
-      name:      name,
-      comments:  "@return [#{type}]",
-      location:  location,
-      closure:   ns,
-      scope:     :instance,
-      attribute: true
-    )
-  end
-
-  def build_location(ast, path)
-    Solargraph::Location.new(
-      path,
-      Solargraph::Range.from_to(
-        ast.location.first_line,
-        0,
-        ast.location.last_line,
-        ast.location.column
-      )
-    )
-  end
-
-  def relation_pins(source_map, ns)
-    ast    = source_map.source.node
-    walker = Walker.new(ast)
-    pins   = []
-
-    walker.on :send, [nil, :belongs_to] do |ast|
-      pins << singular_association(ns, ast)
-    end
-
-    walker.on :send, [nil, :has_one] do |ast|
-      pins << singular_association(ns, ast)
-    end
-
-    walker.on :send, [nil, :has_many] do |ast|
-      pins << plural_association(ns, ast)
-    end
-
-    walker.on :send, [nil, :has_and_belongs_to_many] do |ast|
-      pins << plural_association(ns, ast)
-    end
-
-    walker.walk
-    pins
-  end
-
-  # TODO: handle custom class for relation
-  def plural_association(ns, ast)
-    relation_name = ast.children[2].children.first
-
-    build_public_method(
-      ns,
-      relation_name.to_s,
-      "ActiveRecord::Associations::CollectionProxy<#{relation_name.to_s.singularize.camelize}>",
-      ast:  ast,
-      path: ns.filename
-    )
-  end
-
-  # TODO: handle custom class for relation
-  def singular_association(ns, ast)
-    relation_name = ast.children[2].children.first
-
-    build_public_method(
-      ns,
-      relation_name.to_s,
-      relation_name.to_s.camelize,
-      ast:  ast,
-      path: ns.filename
-    )
-  end
-
-  # TODO: support custom table names, by parsing `self.table_name = ` invokations
-  # inside model
-  def infer_table_name(ns)
-    ns.name.underscore.pluralize
-  end
-
-  def schema
-    @extracted_schema ||= begin
-      ast = Solargraph::Parser.parse_with_comments(File.read("db/schema.rb"), "db/schema.rb")
-      extract_schema(ast)
-    end
-  end
-
-  ColumnData = Struct.new(:type, :ast)
-
-  def extract_schema(ast)
-    schema = {}
-
-    walker = Walker.new(ast)
-    walker.on :block, [:send, nil, :create_table] do |ast, query|
-      table_name = ast.children.first.children[2].children.last
-      schema[table_name] = {}
-
-      query.on :send, [:lvar, :t] do |column_ast|
-        name = column_ast.children[2].children.last
-        type = column_ast.children[1]
-
-        next if type == :index
-        schema[table_name][name] = ColumnData.new(type, column_ast)
-      end
-    end
-
-    walker.walk
-    schema
-  end
 end
 
-Solargraph::Convention.register(SolarRails) # unless ENV["RAILS_ENV"] == "test"
+Solargraph::Convention.register(SolarRails) unless ENV["RAILS_ENV"] == "test"
