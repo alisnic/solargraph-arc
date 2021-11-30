8821ba2f692c8ea2ff66ab1356633697d47e3ee2
Andrei Lisnic <andrei.lisnic@gmail.com>
2021-11-30 23:54:48 +0200

    Move rails api to separate file

--- lib/solar-rails.rb
+++ lib/solar-rails.rb
@@ -7,6 +7,7 @@ require_relative './solar-rails/autoload.rb'
 require_relative './solar-rails/relation.rb'
 require_relative './solar-rails/devise.rb'
 require_relative './solar-rails/walker.rb'
+require_relative './solar-rails/rails_api.rb'
 
 # TODO: remove after https://github.com/castwide/solargraph/pull/509 is merged
 class Solargraph::YardMap
@@ -32,37 +33,9 @@ end
 module SolarRails
   class Convention < Solargraph::Convention::Base
     def global yard_map
-      ann    = File.read(File.dirname(__FILE__) + "/solar-rails/annotations.rb")
-      source = Solargraph::Source.load_string(ann, "annotations.rb")
-      map    = Solargraph::SourceMap.map(source)
-
-      Solargraph.logger.debug("[Rails] found #{map.pins.size} pins in annotations")
-
-      overrides = [
-        Util.method_return("ActionController::Metal#params", "ActionController::Parameters")
-      ]
-
-      ns = Solargraph::Pin::Namespace.new(
-        name:  "ActionController::Base",
-        gates: ["ActionController::Base"]
+      Solargraph::Environ.new(
+        pins: SolarRails::RailsApi.instance.process(yard_map)
       )
-
-      definitions = [
-        Util.build_public_method(
-          ns,
-          "response",
-          types: ["ActionDispatch::Response"],
-          location: Util.dummy_location("whatever.rb")
-        ),
-        Util.build_public_method(
-          ns,
-          "request",
-          types: ["ActionDispatch::Request"],
-          location: Util.dummy_location("whatever.rb")
-        )
-      ]
-
-      Solargraph::Environ.new(pins: map.pins + definitions + overrides)
     end
 
     #<Solargraph::Pin::Reference::Include ``
---
+++ lib/solar-rails/rails_api.rb
@@ -0,0 +1,41 @@
+module SolarRails
+  class RailsApi
+    def self.instance
+      @instance ||= self.new
+    end
+
+    def process yard_map
+      ann    = File.read(File.dirname(__FILE__) + "/annotations.rb")
+      source = Solargraph::Source.load_string(ann, "annotations.rb")
+      map    = Solargraph::SourceMap.map(source)
+
+      Solargraph.logger.debug("[Rails] found #{map.pins.size} pins in annotations")
+
+      overrides = [
+        Util.method_return("ActionController::Metal#params", "ActionController::Parameters")
+      ]
+
+      ns = Solargraph::Pin::Namespace.new(
+        name:  "ActionController::Base",
+        gates: ["ActionController::Base"]
+      )
+
+      definitions = [
+        Util.build_public_method(
+          ns,
+          "response",
+          types: ["ActionDispatch::Response"],
+          location: Util.dummy_location("whatever.rb")
+        ),
+        Util.build_public_method(
+          ns,
+          "request",
+          types: ["ActionDispatch::Request"],
+          location: Util.dummy_location("whatever.rb")
+        )
+      ]
+
+      map.pins + definitions + overrides
+    end
+  end
+end
