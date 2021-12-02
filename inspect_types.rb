106b1196af0b6300ec3e119d04f1c09f4f7c66ca
Andrei Lisnic <andrei.lisnic@gmail.com>
2021-12-02 14:28:20 +0200

    Start

--- inspect_types.rb
+++ inspect_types.rb
@@ -3,6 +3,12 @@ require_relative(ARGV.first + 'config/environment')
 class Model < ActiveRecord::Base
 end
 
-(ActiveRecord::QueryMethods.instance_methods - Object.methods).each do |meth|
-  puts "#{meth} -> #{Model.send(:meth).class}"
+(ActiveRecord::QueryMethods.instance_methods(true) - Object.methods).each do |meth|
+  next if meth.to_s.end_with?("=")
+
+  begin
+    puts "#{meth} \t-> #{Model.send(meth).class}"
+  rescue => error
+    puts "#{meth} \t-> #{error}"
+  end
 end
