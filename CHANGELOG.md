bd81914f78d8938783c5eff50beebe29b13ef951
Andrei Lisnic <andrei.lisnic@gmail.com>
2021-12-20 15:49:33 +0200

    0.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+# 0.3.0
+
+- Relax ActiveSupport dependency
+
 # 0.2.0
 
 Initial alpha
--- lib/solargraph/arc/version.rb
+++ lib/solargraph/arc/version.rb
@@ -1,5 +1,5 @@
 module Solargraph
   module Arc
-    VERSION = "0.2.0"
+    VERSION = "0.3.0"
   end
 end
