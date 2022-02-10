623daeef592afa2d30be3dfd6c0631b3fcba39f7
Andrei Lisnic <andrei.lisnic@gmail.com>
2022-02-10 13:55:23 +0200

    Add spec for Devise in Rails6

--- spec/solargraph-arc/devise_spec.rb
+++ spec/solargraph-arc/devise_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Solargraph::Arc::Devise do
     Solargraph::Convention.register Solargraph::Arc::Convention
   end
 
-  it "includes devise modules" do
+  it "includes devise modules in rails5" do
     map = use_workspace "./spec/rails5" do |root|
       root.write_file 'app/models/awesome_user.rb', <<~RUBY
         class AwesomeUser < ActiveRecord::Base
@@ -34,4 +34,38 @@ RSpec.describe Solargraph::Arc::Devise do
     assert_public_instance_method(map, "ApplicationController#current_awesome_user", ['AwesomeUser', 'nil'])
     assert_public_instance_method(map, "ApplicationController#awesome_user_session", ['undefined'])
   end
+
+  it "includes devise modules in rails6" do
+    map = use_workspace "./spec/rails6" do |root|
+      root.write_file 'app/models/awesome_user.rb', <<~RUBY
+        class AwesomeUser < ActiveRecord::Base
+          devise :registerable, :confirmable, :timeoutable, timeout_in: 12.hours
+        end
+      RUBY
+
+      root.write_file 'app/controllers/pages_controller.rb', <<~RUBY
+        class PagesController < ApplicationController
+          def index
+            curr
+            AwesomeUser.new.conf
+          end
+        end
+      RUBY
+    end
+
+    filename = './app/controllers/pages_controller.rb'
+    expect(completion_at(filename, [2, 7], map)).to include("current_awesome_user")
+    expect(completion_at(filename, [3, 23], map)).to include("confirm")
+
+    assert_public_instance_method(map, "ApplicationController#authenticate_awesome_user!", ['undefined'])
+    assert_public_instance_method(map, "ApplicationController#awesome_user_signed_in?", ['true', 'false'])
+    assert_public_instance_method(map, "ApplicationController#current_awesome_user", ['AwesomeUser', 'nil'])
+    assert_public_instance_method(map, "ApplicationController#awesome_user_session", ['undefined'])
+  end
 end
+
+
+
+
+
+
