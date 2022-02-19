7dd246f5282b7132c4072cefae5ce0aff9bfa611
Stephen Sugden <me@stephensugden.com>
2022-02-19 15:02:20 +0100

    Extract custom class name from association methods

--- lib/solargraph/arc/model.rb
+++ lib/solargraph/arc/model.rb
@@ -44,28 +44,38 @@ module Solargraph
         pins
       end
 
-      # TODO: handle custom class for relation
       def plural_association(ns, ast)
         relation_name = ast.children[2].children.first
+        class_name = extract_custom_class_name(ast) || relation_name.to_s.singularize.camelize
 
         Util.build_public_method(
           ns,
           relation_name.to_s,
-          types: ["ActiveRecord::Associations::CollectionProxy<#{relation_name.to_s.singularize.camelize}>"],
+          types: ["ActiveRecord::Associations::CollectionProxy<#{class_name}>"],
           location: Util.build_location(ast, ns.filename)
         )
       end
 
-      # TODO: handle custom class for relation
       def singular_association(ns, ast)
         relation_name = ast.children[2].children.first
+        class_name = extract_custom_class_name(ast) || relation_name.to_s.camelize
 
         Util.build_public_method(
           ns,
           relation_name.to_s,
-          types: [relation_name.to_s.camelize],
+          types: [class_name],
           location: Util.build_location(ast, ns.filename)
         )
+      end
+
+      def extract_custom_class_name(ast)
+        options = ast.children[3..-1].find { |n| n.type == :hash }
+        return unless options
+
+        class_name_pair = options.children.find do |n|
+          n.children[0].deconstruct == [:sym, :class_name] && n.children[1].type == :str
+        end
+        class_name_pair && class_name_pair.children.last.children.last
       end
     end
   end
